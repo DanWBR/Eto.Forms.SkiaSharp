6cd015dc11b6269b6e3e51f4625f10a7421f1019
DanWBR <danielwag@gmail.com>
2017-08-22 11:22:11 -0400

    changed project to console executable

---
+++ Eto.Forms.Controls.SkiaSharp/Main.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eto.Forms.Controls.SkiaSharp.Tests
+{
+    class Tests
+    {
+
+        static void Main()
+        { 
+        
+        }
+
+    }
+}
